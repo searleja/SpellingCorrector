88a46f1788047e5182da5d7917077227f00f81a3
Jarod Searle <searlejarod@gmail.com>
2021-01-20 21:14:52 -0700

    final changes

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -3,6 +3,8 @@ package spell;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class SpellCorrector implements ISpellCorrector {
   Trie myTrie = new Trie();
@@ -45,6 +47,102 @@ public class SpellCorrector implements ISpellCorrector {
    */
   @Override
   public String suggestSimilarWord(String inputWord) {
-    return null;
+    inputWord = inputWord.toLowerCase();
+    String finalSuggestion = null;
+
+    if (myTrie.find(inputWord) != null) { //word exists in the trie
+      return inputWord;
+    }
+    ArrayList<String> candidates = new ArrayList<>();
+    candidates = deletion(candidates, inputWord);
+    candidates = insertion(candidates, inputWord);
+    candidates = transposition(candidates, inputWord);
+    candidates = alteration(candidates, inputWord);
+
+    ArrayList<String> validWords = new ArrayList<>();
+    for (int i = 0; i < candidates.size(); i++) {
+      if (myTrie.find(candidates.get(i)) != null) {
+        validWords.add(candidates.get(i));
+      }
+    }
+
+    int max = 0;
+    for (int i = 0; i < validWords.size(); i++) {
+      if (myTrie.find(validWords.get(i)).getValue() > max) {
+        max = myTrie.find(validWords.get(i)).getValue();
+        finalSuggestion = validWords.get(i);
+      }
+    }
+    if (finalSuggestion != null) return finalSuggestion;
+
+    //second time
+    ArrayList<String> secondCandidates = new ArrayList<>();
+    for (String word : candidates) {
+      secondCandidates=deletion(secondCandidates, word);
+      secondCandidates=insertion(secondCandidates, word);
+      secondCandidates=transposition(secondCandidates, word);
+      secondCandidates=alteration(secondCandidates, word);
+    }
+
+    validWords = new ArrayList<>();
+    for (int i = 0; i < secondCandidates.size(); i++) {
+      if (myTrie.find(secondCandidates.get(i)) != null) {
+        validWords.add(secondCandidates.get(i));
+      }
+    }
+
+    max = 0;
+    for (int i = 0; i < validWords.size(); i++) {
+      if (myTrie.find(validWords.get(i)).getValue() > max) {
+        max = myTrie.find(validWords.get(i)).getValue();
+        finalSuggestion = validWords.get(i);
+      }
+    }
+
+    return finalSuggestion;
   }
+
+  private ArrayList<String> deletion(ArrayList<String> candidates, String word) {
+    for (int i = 0; i < word.length(); i++) {
+      StringBuilder sb = new StringBuilder(word);
+      sb.deleteCharAt(i);
+      candidates.add(sb.toString());
+    }
+    return candidates;
+  }
+
+  private ArrayList<String> transposition(ArrayList<String> candidates, String word) {
+    for (int i = 0; i < word.length() - 1; i++) {
+      StringBuilder sb = new StringBuilder(word);
+      sb.setCharAt(i, word.charAt(i+1));
+      sb.setCharAt(i+1, word.charAt(i));
+      candidates.add(sb.toString());
+    }
+    return candidates;
+  }
+
+  private ArrayList<String> alteration(ArrayList<String> candidates, String word) {
+    for (int i = 0; i < word.length(); i++) {
+      for (int j = 0; j < 26; j++) {
+        StringBuilder sb=new StringBuilder(word);
+        sb.setCharAt(i, (char)(j+'a'));
+        candidates.add(sb.toString());
+      }
+    }
+    return candidates;
+  }
+
+  private ArrayList<String> insertion(ArrayList<String> candidates, String word) {
+    for (int i = 0; i <= word.length(); i++) {
+      for (int j = 0; j < 26; j++) { //each letter of the alphabet
+        StringBuilder sb = new StringBuilder(word);
+        sb.insert(i, (char)(j+'a'));
+        candidates.add(sb.toString());
+      }
+    }
+    return candidates;
+  }
+
+
+
 }
